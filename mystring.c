fe7e2f945482eb1c008713f5604f11fceb03fccd
fsiniscalco10@stuy.edu <fsiniscalco10@stuy.edu>
2020-10-20 19:28:39 +0200

    updated

--- mystring.c
+++ mystring.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <string.h>
 
 int mystrlen(char *s)
 {
@@ -14,9 +12,39 @@ int mystrlen(char *s)
 	}
 	return l;
 }
-char *mystrcpy(char *dest, char *source) {}
-char *mystrncpy(char *dest, char *source, int n) {}
-char *mystrcat(char *dest, char *source) {}
-char *mystrncat(char *dest, char *source, int n) {}
-int mystrcmp(char *s1, char *s2) {}
-char *mystrchr(char *s, char c) {}
+char *mystrcpy(char *dest, char *source)
+{
+	int i=0;
+	while(source[i]){
+		dest[i]=source[i];
+		i++;
+	}
+	if(source[i]==0)
+		dest[i]=0;
+	return dest;
+}
+char *mystrncpy(char *dest, char *source, int n)
+{
+	int i=0;
+	while(source[i]){
+		if(i>=n)
+			return dest;
+		dest[i]=source[i];
+		i++;
+	}
+	if(source[i]==0 && i<n)
+		dest[i]=0;
+	return dest;
+}
+char *mystrcat(char *dest, char *source)
+{
+}
+char *mystrncat(char *dest, char *source, int n)
+{
+}
+int mystrcmp(char *s1, char *s2)
+{
+}
+char *mystrchr(char *s, char c)
+{
+}
