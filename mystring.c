8efd4288d818754b993763e1cb093dc46c2234ef
fsiniscalco10@stuy.edu <fsiniscalco10@stuy.edu>
2020-10-22 12:09:52 +0200

    finished

--- mystring.c
+++ mystring.c
@@ -45,16 +45,55 @@ char *mystrncpy(char *dest, char *source, int n)
 
 char *mystrcat(char *dest, char *source)
 {
-	dest += mystrlen(dest);
-	if (dest - 1 == 0)
-		dest -= 1;
+	int i = mystrlen(dest);
+	int j = 0;
+	while (source[j])
+	{
+		dest[i] = source[j];
+		j++;
+		i++;
+	}
+	dest[i] = 0;
+	return dest;
 }
+
 char *mystrncat(char *dest, char *source, int n)
 {
+	int i = mystrlen(dest);
+	int j = 0;
+	while (source[j] && j < n)
+	{
+		dest[i] = source[j];
+		j++;
+		i++;
+	}
+	dest[i] = 0;
+	return dest;
 }
+
 int mystrcmp(char *s1, char *s2)
 {
+	int i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] < s2[i])
+			return 1;
+		else if (s1[i] > s2[i])
+			return -1;
+		i++;
+	}
+	return mystrlen(s1)-mystrlen(s2);
 }
+
 char *mystrchr(char *s, char c)
 {
+	int i=0;
+	while(s[i]){
+		if(s[i]==c)
+			return s+i;
+		i++;
+	}
+	if(s[i]==c)
+		return s+i;
+	return 0;
 }
